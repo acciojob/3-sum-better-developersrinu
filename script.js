6b75b2f9f213afe3d8218a5fd7758b43ba630f9e
developersrinu <132943635+developersrinu@users.noreply.github.com>
2023-07-05 14:58:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,34 @@
-function threeSum(arr, target) {
+function nums(arr, target) {
 // write your code here
+	  // Sort the array in ascending order
+  nums.sort((a, b) => a - b);
+
+  let closestSum = Infinity;
+
+  for (let i = 0; i < nums.length - 2; i++) {
+    let left = i + 1;
+    let right = nums.length - 1;
+
+    while (left < right) {
+      const sum = nums[i] + nums[left] + nums[right];
+
+      // Check if the current sum is closer to the target
+      if (Math.abs(sum - target) < Math.abs(closestSum - target)) {
+        closestSum = sum;
+      }
+
+      if (sum < target) {
+        left++;
+      } else if (sum > target) {
+        right--;
+      } else {
+        // Found an exact match, no need to continue
+        return closestSum;
+      }
+    }
+  }
+
+  return closestSum;
   
 }
 
